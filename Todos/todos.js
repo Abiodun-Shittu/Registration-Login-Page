bb41e03f1316c47e68290dafbdb01a205145b484
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-12-02 04:03:44 +0100

    GET, PATCH and DELETE methods implemented

--- Todos/todos.js
+++ Todos/todos.js
@@ -1,6 +1,13 @@
 const username = document.querySelector(".username");
 const todoList = document.querySelector(".todos");
 const todoNum = document.querySelector(".todo-num");
+const visible = document.querySelector(".visible");
+const hidden = document.querySelector(".hidden");
+const updateTodoForm = document.querySelector(".form");
+const titleInput = document.getElementById("title");
+const descriptionInput = document.getElementById("description");
+const dueDateInput = document.getElementById("date");
+const updateButton = document.querySelector(".btn");
 let output = "";
 
 const url = "http://localhost:8000/api/v0/todos/";
@@ -12,9 +19,12 @@ const renderTodos = (todos) => {
 		output += `
 			<div class="todo">
 				<div class="todo-text">
-					<h3>${todo.title}</h3>
-					<p>${todo.description}</p>
-					<p>${todo.due_date.slice(0, 10)} ${todo.due_date.slice(11, 16)}</p>
+					<h3 class="title">${todo.title}</h3>
+					<p class="description">${todo.description}</p>
+					<p class="due_date">${todo.due_date.slice(0, 10)} ${todo.due_date.slice(
+			11,
+			16
+		)}</p>
 				</div>
 				<div class="todo-update" data-id=${todo.todo_id}>
 					<img id="edit" src="../assets/images/edit-icon.png" alt="Edit">
@@ -29,15 +39,17 @@ const renderTodos = (todos) => {
 fetch(url, {
 	method: "GET",
 	headers: {
-		Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU4NmFlZmEzLTNjNmItNDkyYy1hYzE1LWQyYmQzNGMxOGUxOSIsImVtYWlsIjoiamVycnl3aXprbGF5MUBnbWFpbC5jb20iLCJpYXQiOjE2Njk4MTkzNzQsImV4cCI6MTY2OTkwNTc3NH0.oBj9J15GsIhKomLnkVp-B0c9iFWRJBpgvCmlcwdQ5QA`,
+		Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU4NmFlZmEzLTNjNmItNDkyYy1hYzE1LWQyYmQzNGMxOGUxOSIsImVtYWlsIjoiamVycnl3aXprbGF5MUBnbWFpbC5jb20iLCJpYXQiOjE2Njk5MTYyNDMsImV4cCI6MTY3MDAwMjY0M30.ODlvpb7bs9vxZoYqEzcZHKABbDOYTwuo_wquxXQIO9s`,
 	},
 })
 	.then((res) => res.json())
 	.then((data) => {
-		console.log(data[0].todo_id);
 		username.innerHTML = "Abiodun";
 		todoNum.innerHTML = data.length + " todos";
 		renderTodos(data);
+	})
+	.catch((err) => {
+		console.log(err);
 	});
 
 todoList.addEventListener("click", (e) => {
@@ -51,7 +63,7 @@ todoList.addEventListener("click", (e) => {
 		fetch(url + todoId, {
 			method: "DELETE",
 			headers: {
-				Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU4NmFlZmEzLTNjNmItNDkyYy1hYzE1LWQyYmQzNGMxOGUxOSIsImVtYWlsIjoiamVycnl3aXprbGF5MUBnbWFpbC5jb20iLCJpYXQiOjE2Njk4MTkzNzQsImV4cCI6MTY2OTkwNTc3NH0.oBj9J15GsIhKomLnkVp-B0c9iFWRJBpgvCmlcwdQ5QA`,
+				Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU4NmFlZmEzLTNjNmItNDkyYy1hYzE1LWQyYmQzNGMxOGUxOSIsImVtYWlsIjoiamVycnl3aXprbGF5MUBnbWFpbC5jb20iLCJpYXQiOjE2Njk5MTYyNDMsImV4cCI6MTY3MDAwMjY0M30.ODlvpb7bs9vxZoYqEzcZHKABbDOYTwuo_wquxXQIO9s`,
 			},
 		})
 			.then((res) => res.json())
@@ -63,8 +75,49 @@ todoList.addEventListener("click", (e) => {
 					location.reload();
 				}
 			})
-			.catch((error) => {
-				console.log(error);
+			.catch((err) => {
+				console.log(err);
 			});
 	}
+
+	if (editIcon) {
+		setTimeout(() => {
+			visible.classList.add("hidden");
+			hidden.classList.remove("hidden");
+
+			const parent = e.target.parentElement.parentElement;
+			const titleValue = parent.querySelector(".title").textContent;
+			const descriptionValue =
+				parent.querySelector(".description").textContent;
+			const dueDateValue = parent.querySelector(".due_date").textContent;
+
+			titleInput.value = titleValue;
+			descriptionInput.value = descriptionValue;
+			dueDateInput.value = dueDateValue;
+		}, 1500);
+	}
+	updateButton.addEventListener("click", (e) => {
+		e.preventDefault();
+
+		fetch(url + todoId, {
+			method: "PATCH",
+			headers: {
+				Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU4NmFlZmEzLTNjNmItNDkyYy1hYzE1LWQyYmQzNGMxOGUxOSIsImVtYWlsIjoiamVycnl3aXprbGF5MUBnbWFpbC5jb20iLCJpYXQiOjE2Njk5MTYyNDMsImV4cCI6MTY3MDAwMjY0M30.ODlvpb7bs9vxZoYqEzcZHKABbDOYTwuo_wquxXQIO9s`,
+			},
+			body: {
+				"title": "backend Task",
+				"description": "To Build a CRUD API",
+				"dueDate": "22-10-2022",
+			},
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				if (data.message === "Unauthorized") {
+					window.location.href = "../login/login.html";
+				} else if (data.message === "Todo Successfully Updated") {
+					console.log("Todo updated successfully");
+				}
+			});
+	});
 });
